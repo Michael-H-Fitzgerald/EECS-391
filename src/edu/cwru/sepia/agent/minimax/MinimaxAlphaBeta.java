79c39c5ad0878f53200259d6e506124e5c4b2595
SWhelan <sarah@lifeinstillsllc.com>
2016-02-27 01:43:13 -0500

    Skeleton code from textbook for AB search

--- src/edu/cwru/sepia/agent/minimax/MinimaxAlphaBeta.java
+++ src/edu/cwru/sepia/agent/minimax/MinimaxAlphaBeta.java
@@ -1,14 +1,14 @@
 package edu.cwru.sepia.agent.minimax;
 
-import edu.cwru.sepia.action.Action;
-import edu.cwru.sepia.agent.Agent;
-import edu.cwru.sepia.environment.model.history.History;
-import edu.cwru.sepia.environment.model.state.State;
-
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.agent.Agent;
+import edu.cwru.sepia.environment.model.history.History;
+import edu.cwru.sepia.environment.model.state.State;
 
 public class MinimaxAlphaBeta extends Agent {
 
@@ -72,12 +72,58 @@ public class MinimaxAlphaBeta extends Agent {
      * @param beta The current best value for the minimizing node from this node to the root
      * @return The best child of this node with updated values
      */
-    public GameStateChild alphaBetaSearch(GameStateChild node, int depth, double alpha, double beta)
-    {
-        return node;
+    public GameStateChild alphaBetaSearch(GameStateChild node, int depth, double alpha, double beta){
+        double value = maxValue(node, depth, alpha, beta);
+        return getActionWithValue(node.action, value);
     }
+    
+    private GameStateChild getActionWithValue(Map<Integer, Action> action, double value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    /**
+	private double maxValue(GameStateChild node, int depth, double alpha, double beta){
+    	if(cutOffTest(node, depth)){
+    		return node.state.getUtility();
+    	}
+    	double value = Double.NEGATIVE_INFINITY;
+    	for(Action action : node.action.values()){
+    		value = Math.max(value, minValue(result(node.state, action), depth - 1, alpha, beta));
+    		if(value <= alpha){
+    			return value;
+    		}
+    		beta = Math.min(beta, value);
+    	}
+    	return value;
+    }
+    
+	private double minValue(GameStateChild node, int depth, double alpha, double beta) {
+		if(cutOffTest(node, depth)){
+			return node.state.getUtility();
+		}
+		double value = Double.POSITIVE_INFINITY; 
+		for(Action action : node.action.values()){
+			value = Math.min(value, maxValue(result(node.state, action), depth - 1, alpha, beta));
+			if(value <= alpha){
+				return value;
+			}
+			beta = Math.min(beta, value);
+		}
+		return value;
+	}
+
+	private boolean cutOffTest(GameStateChild node, int depth) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+    private GameStateChild result(GameState state, Action action) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	/**
      * You will implement this.
      *
      * Given a list of children you will order them according to heuristics you make up.
